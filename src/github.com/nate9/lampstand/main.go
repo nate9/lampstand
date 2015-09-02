45f80ce518121cca269b079230031be54d185d70
nate9 <lai.nathaniel@gmail.com>
2015-09-02 22:50:46 +1000

    re-add files

---
+++ src/github.com/nate9/lampstand/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type PassageService struct {
+	dao PassageDao
+}
+
+type PassageQuery struct {
+	book    string
+	chapter int
+	begin   int
+	end     int
+}
+
+func NewPassageService(db string) *PassageService {
+	service := new(PassageService)
+	dao, err := NewPassageDao(db)
+	service.dao = dao
+	if err != nil {
+		fmt.Println("couldn't open passage service: ", err)
+	}
+	return service
+}
+
+func (s *PassageService) findVerses(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	params := r.URL.Query()
+	passagequery := params.Get("passage")
+	fmt.Println(passagequery)
+	pq := parsePassage(passagequery)
+	var passage Passage
+	var err error
+	if pq.end != -1 {
+		passage, err = s.dao.FindVerses(pq.book, pq.chapter, pq.begin, pq.end)
+	} else if pq.begin != -1 {
+		passage, err = s.dao.FindVerse(pq.book, pq.chapter, pq.begin)
+	} else {
+		passage, err = s.dao.FindChapter(pq.book, pq.chapter)
+	}
+	checkErr(err)
+	passageJson := ToJson(passage)
+	fmt.Fprint(w, string(passageJson))
+}
+
+func parsePassage(passagequery string) (pq PassageQuery) {
+	pq = PassageQuery{book: "", chapter: -1, begin: -1, end: -1}
+	passagequery = strings.TrimSpace(passagequery)
+	end := len(passagequery)
+	lastspace := strings.LastIndex(passagequery, " ")
+	pq.book = passagequery[:lastspace]
+	colon := strings.Index(passagequery, ":")
+
+	if colon != -1 {
+		pq.chapter, _ = strconv.Atoi(passagequery[lastspace+1 : colon])
+	} else {
+		pq.chapter, _ = strconv.Atoi(passagequery[lastspace+1 : end])
+	}
+
+	hyphen := strings.Index(passagequery, "-")
+	if hyphen != -1 && colon != -1 {
+		pq.begin, _ = strconv.Atoi(passagequery[colon+1 : hyphen])
+		pq.end, _ = strconv.Atoi(passagequery[hyphen+1 : end])
+	} else if hyphen == -1 && colon != -1 {
+		pq.begin, _ = strconv.Atoi(passagequery[colon+1 : end])
+	}
+
+	return pq
+}
+
+func main() {
+	service := NewPassageService("./hcsb.db")
+	router := httprouter.New()
+	router.GET("/api/verses", service.findVerses)
+	http.ListenAndServe(":8080", router)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+}
